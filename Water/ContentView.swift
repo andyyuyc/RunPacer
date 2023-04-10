5897cc86728d008cbe76e7cce8a8de3dfa2b3b8a
andyyuyc <76409596+andyyuyc@users.noreply.github.com>
2023-04-10 15:53:40 -0500

    Revert "Version 1.0.1"
    This reverts commit e65cc4407b1219b61972f78c39f31d9de873c539.

--- Water/ContentView.swift
+++ Water/ContentView.swift
@@ -1,48 +1,21 @@
+//
+//  ContentView.swift
+//  Water
+//
+//  Created by Andy Yu on 2023/4/10.
+//
+
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selection = 0
-    
     var body: some View {
-        TabView(selection: $selection) {
-            
-            Home()
-                .tabItem {
-                    Image(systemName: "drop.degreesign")
-                    Text("喝水")
-                }
-                .tag(0)
-            
-            Text("紀錄")
-                .tabItem {
-                    Image(systemName: "clock.fill")
-                    Text("紀錄")
-                }
-                .tag(1)
-            
-            NavigationView {
-                Sport()
-            }
-            .tabItem {
-                Image(systemName: "figure.run")
-                Text("運動")
-            }
-            .tag(2)
-            
-            
-            NavigationView {
-                //Setting()
-            }.tabItem {
-                Image(systemName: "gearshape")
-                Text("設定")
-            }.tag(3)
-            
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
         }
-        .accentColor(.white)
-        .onAppear {
-            UITabBar.appearance().unselectedItemTintColor = .black // 未選取的顏色
-        }
-        .background(Color.red)
+        .padding()
     }
 }
 

426e5a59529bc85d8e9fc7b180fbc36fa9d3a1a5
andyyuyc <76409596+andyyuyc@users.noreply.github.com>
2023-04-10 14:50:46 -0500

    Initial Commit

---
+++ Water/WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApp.swift
+//  Water
+//
+//  Created by Andy Yu on 2023/4/10.
+//
+
+import SwiftUI
+
+@main
+struct WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
